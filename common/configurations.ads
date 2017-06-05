6c7b0df4f6970105c8ee7edf1183083e74a37433
Daniel King <damaki.gh@gmail.com>
2017-06-05 15:59:38 -0300

    Add copyright header and license

--- common/configurations.ads
+++ common/configurations.ads
@@ -1,12 +1,37 @@
+-------------------------------------------------------------------------------
+--  Copyright (c) 2017 Daniel King
+--
+--  Permission is hereby granted, free of charge, to any person obtaining a
+--  copy of this software and associated documentation files (the "Software"),
+--  to deal in the Software without restriction, including without limitation
+--  the rights to use, copy, modify, merge, publish, distribute, sublicense,
+--  and/or sell copies of the Software, and to permit persons to whom the
+--  Software is furnished to do so, subject to the following conditions:
+--
+--  The above copyright notice and this permission notice shall be included in
+--  all copies or substantial portions of the Software.
+--
+--  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+--  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+--  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+--  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+--  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+--  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+--  DEALINGS IN THE SOFTWARE.
+-------------------------------------------------------------------------------
+
 with DecaDriver.Core;
 with EVB1000.S1;
 
 package Configurations
+  with SPARK_Mode => On
 is
 
    procedure Get_Switches_Config
      (Config : out DecaDriver.Core.Configuration_Type)
    with Global => (Input => EVB1000.S1.Switch_State),
-   Depends => (Config => EVB1000.S1.Switch_State);
+     Depends => (Config => EVB1000.S1.Switch_State);
+   --  Get the DW1000 user configuration by reading the state of the S1 switch
+   --  on the EVB1000.
 
 end Configurations;
